5b2ae4a5f303c5c187f06833727da220f198b1a9
Ecxxlipse <arnold219@morrisville.edu>
2025-09-30 22:42:51 -0400

    Added rotating obstacles with AddTorque.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,14 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-        InputAction moveAction;
+    [SerializeField] float torqueAmount = 1f;
+    InputAction moveAction;
+    Rigidbody2D myRigidbody2D;
     void Start()
     {
 
         moveAction = InputSystem.actions.FindAction("Move");
+        myRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
 
@@ -15,6 +18,13 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 moveVector;
         moveVector = moveAction.ReadValue<Vector2>();
-        print(moveVector);
+        if (moveVector.x < 0)
+        {
+            myRigidbody2D.AddTorque(torqueAmount);
+        }
+        else if (moveVector.x > 0)
+        {
+            myRigidbody2D.AddTorque(-torqueAmount);
+        }
     }
 }
